4ee6cfbc350fb723b16319a4e24cbce55544a515
Thiago Vieira Ramos <thiago.vramos@hotmail.com>
2024-04-19 17:44:18 -0300

    new-backend
    ** Alterações Primarias**
    - Adição do framework SpringBoot, com as dependências ( Lombok, DevTools, Spring Boot Web).

---
+++ projetoa3/src/test/java/br/com/projetoa3/projetoa3/Projetoa3ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.projetoa3.projetoa3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projetoa3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
