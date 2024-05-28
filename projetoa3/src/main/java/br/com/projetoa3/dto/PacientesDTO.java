937e0e649c405bcba964109f5d7995e09660f964
Thiago Vieira Ramos <thiago.vramos@hotmail.com>
2024-05-28 06:20:35 -0300

    -> UPDATE PROJETO 28/05 <-
    * Alterações principais:
    - Inclusão da classe Serializer para tratamento de request na hora de envio no PostMan, podendo ser enviado horário sem o T.
    - Inclusão da classe Deserializer para tratamento do response da requisão do PostMan,recebendo a data e hora sem o T.
    - Incrementação das annotations de Deserialize/Serialize para especificar onde deverá ser tratado os dados
    -
    
    * Alterações secundárias:
    - N/A

--- projetoa3/src/main/java/br/com/projetoa3/dto/PacientesDTO.java
+++ projetoa3/src/main/java/br/com/projetoa3/dto/PacientesDTO.java
@@ -1,5 +1,9 @@
 package br.com.projetoa3.dto;
 
+import br.com.projetoa3.exception.LocalDateTimeDeserializer;
+import br.com.projetoa3.exception.LocalDateTimeSerializer;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,7 +19,11 @@ public class PacientesDTO {
     private String nome;
     private Integer idade;
     private String sintomas;
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
     private LocalDateTime dataEntrada;
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
     private LocalDateTime dataSaida;
     private String remediosReceitados;
 }
---
+++ projetoa3/src/main/java/br/com/projetoa3/exception/LocalDateTimeDeserializer.java
@@ -0,0 +1,20 @@
+package br.com.projetoa3.exception;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+
+public class LocalDateTimeDeserializer extends JsonDeserializer<LocalDateTime> {
+
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+    @Override
+    public LocalDateTime deserialize(JsonParser p, DeserializationContext ctxt) throws IOException {
+        String dateString = p.getText();
+        return LocalDateTime.parse(dateString, FORMATTER);
+    }
+}
